ad81773a04a9e4f0d22f4db83dc333ece2b09823
Blake-Stephenson <49764055+Blake-Stephenson@users.noreply.github.com>
2022-04-14 00:42:58 -0400

    recursive cell body

--- Frontend/src/cells.ts
+++ Frontend/src/cells.ts
@@ -13,45 +13,46 @@ export  default class CellBasic {
 
 	#speed: number;
 	#heading: number;
-    #skin_color: any;
-    #size: number
+
+    #body: any;
+
 
     stage_width: number;
     stage_height: number;
     #hitbox_range: number;
 
-	constructor(pos_x: number, pos_y: number, s: number, dir: number, color: any, radius: number, max_width: number, max_height: number) {
+	constructor(pos_x: number, pos_y: number, s: number, dir: number, body: any, max_width: number, max_height: number) {
 		this.#position_x = pos_x;
 		this.#position_y = pos_y;
 		this.#speed = s;
 		this.#heading = dir;
-        this.#skin_color = {
-            r: color.r,
-            g: color.g,
-            b: color.b
-        }
+        this.#body = body;
 
-        this.#size = radius;
 
         this.stage_width = max_width;
         this.stage_height = max_height;
         this.#hitbox_range = 1;
 
         this.#graphic = new Graphics();
-        this.#graphic.beginFill((this.#skin_color.r << 16) + (this.#skin_color.g << 8) + this.#skin_color.b);
-        this.#graphic.drawCircle(0,0,this.#size);
+        //this.#graphic.beginFill(this.#skin_color);
+        //this.#graphic.drawCircle(0,0,this.#size);
+
+        //new drawing alg
+        this.draw(this.#graphic,this.#body);
 	}
 
     check_border() {
         //check if hitting wall x
-        if(this.#position_x-this.#size-this.#hitbox_range < 0 || this.#position_x+this.#size+this.#hitbox_range > this.stage_width) {
+        if(this.#position_x-this.#body[1]-this.#hitbox_range < 0 || this.#position_x+this.#body[1]+this.#hitbox_range > this.stage_width) {
             //bounce
             this.#heading = Math.PI-this.#heading;
+            this.#graphic.rotation = this.#heading;
         }
         //check if hitting wall y
-        if(this.#position_y-this.#size-this.#hitbox_range < 0 || this.#position_y+this.#size+this.#hitbox_range > this.stage_height) {
+        if(this.#position_y-this.#body[1]-this.#hitbox_range < 0 || this.#position_y+this.#body[1]+this.#hitbox_range > this.stage_height) {
             //bounce
             this.#heading = 2*Math.PI-this.#heading;
+            this.#graphic.rotation = this.#heading; 
         }
     }
 
@@ -67,6 +68,51 @@ export  default class CellBasic {
         this.#graphic.position.y = this.#position_y
 	}
 
+    drawRecursive(cell: Graphics, data: any, x_off: number, y_off: number, theta: number, level: number){
+        for (let i = 0; i < data[3*level-3]; i++)
+            {	
+                let mag = (data[3*level-2]+data[3*level+1]+2*data[3*level-3])/2.5;
+                (data[3*level-3] === 3) ? mag *= 0.8: 0 ;
+                let x = mag* Math.cos((i*360/data[3*level-3]+theta)*Math.PI/180);
+                let y = mag* Math.sin((i*360/data[3*level-3]+theta)*Math.PI/180);
+    
+    
+                let magN = (data[3*level-2]+data[3*level+1]+2*data[3*level-3])/2;
+                (data[level*3-3] === 3) ? magN *= 0.8: 0 ;
+                if ((level*3+3) < data.length){
+                    let phi = (i*360/data[3*level-3])*Math.PI/180
+                    //magN*Math.cos(phi)+	magN*Math.sin(phi)+
+                    this.drawRecursive(cell,data,x_off+x,y_off+y,i*360/data[level*3-3]+(data[level*3]-2)*180/data[level*3]+theta,level+1);
+                }
+    
+                cell.beginFill(data[level*3+2]);
+                //cell.drawRect(x+x_off,y+y_off,300,2);
+                //cell.drawRect(x+x_off,y+y_off,2,300);
+                if (data[3*level]<3)
+                {
+                    cell.drawCircle(x+x_off,y+y_off,data[3*level+1]);
+                }
+                else{
+                    cell.drawRegularPolygon?.(x+x_off,y+y_off,data[3*level+1],data[3*level],(i*360/data[3*level-3]-(90-180/data[3*level])+theta)*Math.PI/180);
+                }
+            }
+    }
+    
+    draw(cell: Graphics, data: any){
+        this.drawRecursive(cell,data,0,0,0,1);
+    
+        cell.beginFill(data[2]);
+        //cell.endFill();
+        if (data[0] < 3)
+        {
+            cell.drawCircle(0, 0, data[1]);
+        }
+        else
+        {
+            cell.drawRegularPolygon?.(0,0,data[1],data[0],(90-180/data[0])*Math.PI/180);
+        }
+    }
+
 	get_pos_x() {
 		return this.#position_x;
 	}
@@ -75,28 +121,30 @@ export  default class CellBasic {
 		return this.#position_y;
 	}
 
-    get_size() {
-        return this.#size;
+
+
+    get_speed(){
+        return this.#speed;
     }
 
     get_graphic() {
         return this.#graphic;
     }
 
-    get_traits() {
-        return {
-            size: this.#size,
-            speed: this.#speed,
-            color: this.#skin_color
-        }
+
+
+    get_body(){
+        return this.#body;
     }
 
     set_color(color: any){
+        color;
+        /*
         if(color != this.#skin_color){
             this.#skin_color = color;
-            this.#graphic.beginFill((this.#skin_color.r * 256 * 256) + (this.#skin_color.g * 256) + this.#skin_color.b);
+            this.#graphic.beginFill(this.#skin_color);
             this.#graphic.drawCircle(0,0,this.#size);
         }
-            
+          */  
     }
 }
--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,4 +1,5 @@
-import { Application } from 'pixi.js';
+import { Graphics, Application  } from 'pixi.js';
+import '@pixi/graphics-extras';
 import CellBasic from './cells';
 import getData from './dblink';
 
@@ -13,19 +14,34 @@ const app = new Application({
 	resizeTo: window
 });
 
-/*  old definition of a cell
-let obj = new Graphics();
+/*
+obj.beginFill(0x1100BB);
+buildshape2(obj,shape0,size0,shape1,size1,shape2,size2);
+
+obj.beginFill(0x66AA33);
+buildshape1(obj,shape0,size0,shape1,size1,0,0,0);
 obj.beginFill(0xff0000);
-obj.drawCircle(300, 300, 20);
+buildshape0(obj,shape0,size0);
 
-app.stage.addChild(obj);
 
-let xSpeed = 0;
-let ySpeed = 1;
+let num1 = 0x224477;
+let num2 = 0x109923;
+
+console.log(num1.toString(16));
+console.log(num2.toString(16));
+
+
+console.log((((num1+num2)&0x1fefeff)/2).toString(16))
+//buildshape0(obj,shape1,size1);
+
 */
+
+
 //get stage size
 const stage_width = window.innerWidth;
 const stage_height = window.innerHeight;
+
+
 
 //create cell from new class
 
@@ -34,22 +50,14 @@ const x = 200;
 const y = 200;
 let speed = 2;
 let heading = Math.PI*0.4; //RADIANS DO NOT FORGET
-let red = {
-	r: 255,
-	g: 0,
-	b: 0
-};
 
-let blue = {
-	r: 0,
-	g: 0,
-	b: 255
-};
-
-let size = 50;
 //create cells
-let obj1 = new CellBasic(x,y,speed,heading, red, size, stage_width,stage_height);
-let obj2 = new CellBasic(100,500,7,Math.PI*1.6, blue,80,stage_width,stage_height);
+let body = [7,60,0x116644,5,30,0x551100,2,20,0x11AA22,3,10,0x334400,3,10,0x330088];
+let obj1 = new CellBasic(x,y,speed,heading, body, stage_width,stage_height);
+
+body = [4,60,0x712624,3,30,0x3511c0,2,20,0x112222,3,10,0x834400];
+
+let obj2 = new CellBasic(100,500,7,Math.PI*1.6, body, stage_width,stage_height);
 
 let num_rendered = 0;
 let objs:CellBasic[] = [obj1,obj2];
@@ -70,40 +78,39 @@ requestAnimationFrame(update);
 
 // Performs 1 pt crossover on 2 parent cells
 // For Now: returns a new cell
-function crossover(parent1: CellBasic, parent2: CellBasic) {
+function coitus(p1: CellBasic, p2: CellBasic){
 
-	let parent1Traits = parent1.get_traits();
-	let parent2Traits = parent2.get_traits();
+	let body1 = p1.get_body();
+	let body2 = p2.get_body();
 
-	// Random int between 0 & 1
-	let traitToSwap: number = Math.floor((Math.random() * (1 - 0 + 1)))
-
-	// Only swap between speed & size traits, colour will be a mix
-	// For now, only swaps opposite traits (NOTE: Need to add more traits for variety)
-	let newCellData = {
-		size: (traitToSwap === 1 ? parent1Traits.size : parent2Traits.size),
-		speed: (traitToSwap === 0 ? parent1Traits.speed : parent2Traits.speed),
-		color: blendColours(parent1Traits.color, parent2Traits.color)
-	}
-	console.log(newCellData.color);
+	//set lenght of body randomly
+	let bodynew = (Math.floor((Math.random() * (1 - 0 + 1))) === 0 ? body1 : body2);
+	let end = Math.min(body1.length,body2.length);
 	
-
-	// Generate a ranom direction to go in
-	// NOTE: random() * (max - min + 1)
+	for (let i = 0; i < bodynew.length; i++){
+		if(i < end){
+			switch (i%3){
+				case 0:
+					bodynew[i] = (Math.floor((Math.random() * (1 - 0 + 1))) === 0 ? body1[i] : body2[i]);
+					//1:10 times to go up one or down one
+					bodynew[i] += Math.floor((Math.random() * (11)-1)/9)
+					bodynew[i] = bodynew[i] < 1 ? 1: bodynew[i];
+					break;
+				case 1:
+					bodynew[i] = (Math.floor((Math.random() * (1 - 0 + 1))) === 0 ? body1[i] : body2[i]);
+					bodynew[i] += 2*Math.floor((Math.random() * (13)-2)/9);
+					break;
+				case 2:
+					//parseInt('0xfae3', 16)  
+					bodynew[i] = (((body1[i]+body2[i])&0x1fefeff)/2)
+			}
+			
+		}
+	}
 	let randAngle = Math.PI * (Math.random() * (2 - 0 + 1))
+	let speednew = (Math.floor((Math.random() * (1 - 0 + 1))) === 0 ? p1.get_speed() : p2.get_speed());
 
-	return new CellBasic(x, y, newCellData.speed, randAngle, newCellData.color, newCellData.size, stage_width, stage_height)
-
-}
-
-
-// Blends 2 colours
-function blendColours(colour1: any, colour2: any) {	
-	return {
-		r: (colour1.r + colour2.r) / 2,
-		g: (colour1.g + colour2.g) / 2,
-		b: (colour1.b + colour2.b) / 2
-	};
+	return new CellBasic(x,y,speednew,randAngle, bodynew, stage_width, stage_height);
 }
 
 // mates 2 random parents
@@ -122,7 +129,7 @@ function crossoverButton(){
 
 	console.log("Mating cells " + randParent1 + " & " + randParent2);
 
-	objs.push(crossover(objs[randParent1], objs[randParent2]));
+	objs.push(coitus(objs[randParent1], objs[randParent2]));
 
 }
 
@@ -169,6 +176,9 @@ function update() {
 	}
 
 	
+	//change pos of test cell
+
+
 	app.render();
 
 	requestAnimationFrame(update);
